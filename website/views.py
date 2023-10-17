66663253a6a35183cdf20f77a1cc45a3a46d3e21
Mr.BoO <56259930+Master-MonSter@users.noreply.github.com>
2023-10-17 17:28:15 +0330

    Initialize (templates & statics)
    +Initialize files about template and static files
    +Create a test(test/test.html, css/test.css) for testing

--- website/views.py
+++ website/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def test(request):
-    return HttpResponse("<h1>Hello</h1>")
+def test_view(request):
+    return render(request, 'test/test.html')
