d6b7a8492e1a5b197e282e79a7f25b1c0878bc29
Mr.BoO <56259930+Master-MonSter@users.noreply.github.com>
2023-10-21 10:31:20 +0330

    Send context with render
    +Example of send a value with render
    +Some edits

--- website/views.py
+++ website/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index_view(request):
-    return render(request, 'website/index.html')
+    context = {'name': 'Soheil', 'lastName': 'Elahifar ', 'email': 'l.o0of.war@gmail.com', 'phoneNumber': '+989385818676'}
+    return render(request, 'website/index.html', context)
 
 def pDetail_view(request):
     return render(request, 'website/portfolio-details.html')
