3dff357514f59368514d892a2dc4e9d37f313a8e
Mr.BoO <56259930+Master-MonSter@users.noreply.github.com>
2023-10-22 10:50:00 +0330

    Start DB
    +Created Contact DB
    fields(name, subject, email, create and update_date)

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from website.models import Contact
+
+class ContactAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    list_display = ('name', 'subject', 'email', 'created_date')
+    list_filter = ('email',)
+    search_fields = ('name','email')
 
 # Register your models here.
+admin.site.register(Contact, ContactAdmin)
